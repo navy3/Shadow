af1e884f68d00fc951a69af6f55d92194f2a07fe
wenwen xie <focusspeed@gmail.com>
2012-12-24 17:09:39 +0800

    init project

---
+++ ShadowDemo/ShadowDemo/TAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TAAppDelegate.h
+//  ShadowDemo
+//
+//  Created by navy on 12-12-24.
+//  Copyright (c) 2012年 navy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShadowDemo/ShadowDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShadowDemo
+//
+//  Created by navy on 12-12-24.
+//  Copyright (c) 2012年 navy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TAAppDelegate class]));
+    }
+}
